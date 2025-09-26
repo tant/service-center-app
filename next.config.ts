b687872fbd368b6da4fffb431a6876c71afa0d9f
Arvin <211909842+arvin-ux@users.noreply.github.com>
2025-09-26 13:26:33 +0700

    feat: add dashboard, add UI components for separator, sheet, sidebar, skeleton, sonner, table, tabs, toggle group, and tooltip
    - Implemented Separator component for visual separation in UI.
    - Created Sheet component for modal-like overlays with header, footer, and content sections.
    - Developed Sidebar component with context for managing open/closed state and mobile responsiveness.
    - Added Skeleton component for loading placeholders.
    - Integrated Sonner for toast notifications with theme support.
    - Built Table component with header, body, footer, and cell functionalities.
    - Established Tabs component for tabbed navigation.
    - Introduced Toggle and ToggleGroup components for toggle button functionality.
    - Created Tooltip component for displaying additional information on hover.
    - Added useIsMobile hook for responsive design handling.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["https://service-center.tantran.dev"],
 };
 
 export default nextConfig;
